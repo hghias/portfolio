304a81087348c4450d07f7bc4a69b512319be9e4
hghias <hghias@umich.edu>
2019-01-06 21:12:13 -0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
 <div class="container-fluid">
-	<div class="row p-5" >
-		<div class="col-6 pl-5"> 
-			<a class="mr-3" href="mailto:hghias@umich.edu"> 
-				<img id="email-icon" class= "img-fluid" src="email.png" width="46" alt="Email" > 
+	<div class="row pt-5 pb-4" >
+		<div class="col-6"> 
+			<a class="pr-1" href="mailto:hghias@umich.edu"> 
+				<img id="email-icon" class= "footer " src="email1.png" alt="Email" > 
 			</a>
 
-			<a class="pr-3" href="https://www.linkedin.com/in/hafsa-ghias/" target="_blank"> 
-				<img id="linkedin-icon" class= "img-fluid" src="linkedin.png" width="30" alt="Linkedin" > 
+			<a class="pr-1" href="https://www.linkedin.com/in/hafsa-ghias/" target="blank"> 
+				<img id="linkedin-icon" class= "footer " src="linkedin.png" alt="Linkedin" > 
 			</a>
 
-			<a class="pr-3" href="https://www.facebook.com/HafsaGphotography/" target="_blank"> 
-				<img id="linkedin-icon" class= "img-fluid" src="facebook.png" width="30" alt="Facebook" > 
+			<a class="pr-1" href="https://www.facebook.com/HafsaGphotography/" target="blank"> 
+				<img id="linkedin-icon" class= "footer " src="facebook.png" alt="Facebook" > 
 			</a>
 
-			<a class="pr-3" href="https://www.instagram.com/hafsagphotography/" target="_blank"> 
-				<img id="instagram-icon" class= "img-fluid" src="instagram.png" width="30" alt="Instagram" > 
+			<a class="pr-1" href="https://www.instagram.com/hafsagphotography/" target="blank"> 
+				<img id="instagram-icon" class= "footer  " src="instagram.png" alt="Instagram" > 
 			</a>
 		</div>
-		<div class=" footer col-6 text-right align-bottom pr-5">
-			<p>Website design © 2018 Hafsa Ghias.</p>
+		<div class=" footer-text col-6 text-right align-bottom">
+			<p>Website design © 2019 Hafsa Ghias.</p>
 		</div>	
 
 	</div>
