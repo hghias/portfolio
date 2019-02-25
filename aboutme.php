db07478dbc80bd4e65ce5837ad24203a2667e494
hghias <hghias@umich.edu>
2019-02-25 18:19:21 -0500

    Add files via upload

--- aboutme.php
+++ aboutme.php
@@ -33,57 +33,52 @@
 	</div>
 </div>
 !-->
-<div class="container-fluid" data-aos="fade-up" data-aos-duration="1000">
-	<div class="row pl-5 pr-5 mx-auto my-auto">
-		<div class="col-8 pl-5 about mx-auto my-auto">
-			<div class="pl-5"> 
-				<h2> Hi, I'm Hafsa! </h2>
+<div class="container" data-aos="fade-up" data-aos-duration="1000">
+	<div class="row">
+		<div class="col-8">
+			<h2> Hi, I'm Hafsa! </h2>
+			</br>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-9">
+			<h5 style= "line-height: 150%;">
+				I’m currently a senior at the University of Michigan studying art & design with a focus on user experience design. I started my college career in software development, but changed my focus when I fell in love with design and its potential to change the world. Engineering makes things work, but design impacts if, how, and why we use something. 
 				</br>
-				<h5 style= "line-height: 150%;">
-				I’m currently a senior at the University of Michigan studying art & design with a focus on user experience design. I started my college career in software development, but changed the course of my path when I fell in love with design and its potential to change the world. Engineering makes things work, but design impacts if, how, and why we use something. 
 				</br>
-				</br>
-				In all the projects that I pursue, I ask <I> “How can we maximize this design to be intuitive, functional, inclusive & aesthetic?” </I>
+				I aim to use design as a tool to foster inclusivity & create equal opportunity. With a background in computer science, my goal is to design user interfaces in regards to the limitations that arise in the development phase. In all the projects that I pursue, I ask <I> “How can we maximize this design to be intuitive, functional, inclusive & aesthetic?” </I>
 				</h5>
 				</br>
 				<a class="btn pn" id="designbutton" role="button" href="Resume_Hafsa_Ghias.pdf" target="blank"> Resume </a>
-				<a class="btn 3d" id="designbutton" role="button" > Contact Me </a>
-			</div>
 		</div>
-		<div class="col-4 mx-auto my-auto">
-			<img class="me" src="IMG_75131.jpg" alt="" style="box-shadow: 0 0 50px 0 rgba(51, 51, 51, 0.3);">
-		</div>
-
-	</div>
-	<div class="row pl-5 pr-5">
-		<div class="col about pt-5 pl-5 pr-5">
-			<div class="pl-5 pr-5 ml-5 mr-5" >
-				<h2> Design Philosophy </h2>
-				</br>
-				<h5 style= "line-height: 150%;">Design is all around us. But the best products, physical or digital, are ones that you don’t realize have been designed, because they've been seamlessly integrated into our lives.
-				</br>
-				</br>
-				Take the design of a door for instance. You use doors everyday. The act of opening and closing a door is a mundane one and is gone unnoticed if it’s intuitive – a quality of good design. However, if it’s too heavy to open or push or pull, the door becomes an obstacle and is considered poor design. Additionally, one must think about the different types of users for a given design as well. What may be an easy door for able-bodied users could serve as a difficulty for users in wheelchairs if there’s no handicap door opener. Understanding the target audience of your brand, digital interface, or product and adapting your design so it’s inclusive and accessible to everyone is every designer’s responsibility.
-				</br>
-				</br>
-				I strive to create intuitive, inclusive and simple design by understanding the client's goals and user's needs.  
-				</h5>
-			</div>
+		<div class="col">
+			<img class="me float-right" src="IMG_75131.jpg" alt="" style="box-shadow: 0 0 50px 0 rgba(51, 51, 51, 0.3);">
 		</div>
 	</div>
-
-	<div class="row pl-5 pr-5">
-		<div class="col about pt-5 pl-5 pr-5">
-			<div class="pl-5 pr-5 ml-5 mr-5" >
-				<h2> My Design Process </h2>
-				</br>
-				<h5 style= "line-height: 150%;">  
-				
-				</h5>
-				<img src="DesignProcess-25.png" alt="" style="width:50%;">
+		
+	<div class="row">
+		<div class="col">
+			</br>
+			</br>	
+			<h2> Design Philosophy </h2>
+			</br>
+			<h5 style= "line-height: 150%;">Design is all around us. But the best products, physical or digital, are ones that you don’t realize have been designed, because they've been seamlessly integrated into our lives. Because they've been designed with the user in mind. they offer an effective solution to a problem you may or may not be aware of or make your day to day life more efficient.
+			</br>
+			</br>
+			Take the design of a door for instance. You use doors everyday. The act of opening and closing a door is a mundane one and is gone unnoticed if it’s intuitive – a quality of good design. However, if it’s too heavy to open or push or pull, the door becomes an obstacle and is considered poor design. Additionally, one must think about the different types of users for a given design as well. What may be an easy door for able-bodied users could serve as a difficulty for users in wheelchairs if there’s no handicap door opener. Understanding the target audience of your brand, digital interface, or product and adapting your design so it’s inclusive and accessible to everyone is every designer’s responsibility.
+			</h5>
+			</br>
+			</br>
+			
+			<h2> My Design Process </h2>
+			</br>
+			<h5 style= "line-height: 150%;"> I believe that process is extremely important because it makes sure I am intentional with every decision. My design process has been abstracted from the UX design process, but can be applied to any project I am working on, whether it being product design, branding design or graphic design. Good design is human centered, which means that it is empathetic, so conducting user research is one of the most important steps. I strive to understand the client's goals and user's needs and translate them into a visual that satisfies the client and user.</h5>
+ 			<div class="my-auto align-self-center text-center"> 
+				<img src="processDetails.png" alt="" style="width:100%;">
 			</div>
 		</div>
 	</div>
+</div> 
 
 </div>
 <?php include ('footer.php');?>
