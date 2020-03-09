67fe969817cb9669843e02d8229938ce1934646d
[Hafsa Ghias] <[hghias@umich.edu]>
2020-03-09 02:33:57 -0400

    Added Munchies

--- Munchies.php
+++ Munchies.php
@@ -10,24 +10,21 @@
         <a href="design.php" style="color: #255484 !important;"> <h2>x</h2> </a>
     </div>
 
-<div class="mt-5"> 
-    <img class="pt-lg-3 pl-lg-5 pr-lg-5 p-3" src="Flutter-Hero-12.png" alt="" style="width:100%;">
+<div class="mt-5 mb-5 "> 
+    <img class="pt-lg-3 pl-lg-5 pr-lg-5 p-3" src="Munchies_Hero.png" alt="" style="width:100%;">
     </br>
     </br>
     <div class="padding-left padding-right">
-        </br>
         <div class="row"> 
     		<div class="col-lg-3">
     			<h5><b>Role</b></h5>
-    			<p>UX Research & Design Intern</p>
+    			<p>Product & Graphic Designer</p>
     			<p></p>
     		</div>
     		<div class="col-lg-3" >
     			<h5><b>Deliverables</b></h5>
     			<p> Physical Prototype </br> 
-                Packaging </br>
-                Logo & Brand Strategy
-                </p>
+                Branding & Packaging </p>
     		</div>
     		<div class="col-lg-3">
     			<h5><b>Timeline</b></h5>
@@ -38,23 +35,112 @@
     	</br>
     	</br>
 
-        <h3 class="text-center"> <b> Strategize </b> </h3>
-        <h5> <b> The Challenge</b></h5>
-        <p> Transform a rock into a companion that leverages cuteness and the delightful design principles to induce or support a behavioral change that benefits the user's mental, physical or emotional well-being. </p>
+        <h4> <b> The Challenge</b></h4>
+        <h4 style="line-height:35px;"> Transform a rock into a companion that leverages cuteness and the <b>delightful design principles</b> (engagement, surprise, liveliness, cuteness, serendipity, and reassurance) to induce or support a behavioral change that benefits the user's mental, physical or emotional well-being. </h4>
+        </br>
+
+        <h4> <b> Product Brief </b></h4>
+        <h4 style="line-height:35px;"> problem statement, target audience, product definition </br>
+        It's vital for children of the age ____ to develop their emotional vocabulary. label emotions and play games with new feeling words. Parents can assign different emotions to the different color jellybeans.  Developing an emotional bond with the product encourages consistent behavior</h4>
+        </br>
+
+        <h4> <b> Initial Research </b></h4>
+
+        <div class="row">
+            <div class="col-6">
+                <p> To figure out what type of wellbeing I wanted to tackle with my product, I turned to the book Positive Computing: Technology for Wellbeing and Human Potential by Dorian Peters and Rafael A. Calvo.</p>
+                <p>The book outlined the different models of wellbeing. The ones that were most interesting to me were:</p>
+                <ul>
+                    <li> <b> Hedonic Psychology:</b> Wellbeing as the experience of positive emotion </li>
+                    <li> <b> Subjective Wellbeing:</b> Consists of life satisfaction, the presence of positive mood and the absence of negative mood. </li>
+                    <li> <b> Eudaimonic Psychology:</b> Wellbeing as engagement with meaning and the fulfillment of potentials</li>
+                </ul>
+
+                <p>The different approaches to designing positive computing are:</p>
+                <ul>
+                    <li> <b>None:</b> Wellbeing is not systematically considered in the design of the technology</li>
+                    <li><b>Preventative:</b> Obstacles or compromises to wellbeing are treated as errors and trigger redesign</li>
+                    <li><b>Active:</b> Technology is designed to actively support components of wellbeing or human potential in an application that has a different overall goal</li>
+                    <li> <b>Dedicated:</b> A technology that is purpose built and dedicated to fostering wellbeing and/or human potential in some way.</li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <img src="MunchiesResearch.jpg" alt="" style="width:100%; border: 1px solid #DEDDDD;">
+            </div>
+        </div>
+        <p> The wellbeing factors that I'm interested in incorporating into my product are positive emotions, motivation & engagement, self-awareness and mindfulness, through a preventative or active positive computing approach. </p>
+        </br>
+
+        <h4> <b> Sketches </b></h4>
+        <p> By understanding the models of wellbeing, the factors that impact wellbeing and the approaches to designing positive computing, I narrowed down what type of problems I wanted to tackle. My initial sketches tackle preventative or active aspects to improving emotional health and mindfulness. Through multiple sessions of critique, I narrowed my idea based on impact and feasibility to building a mood tracker and to cutify it, I decided to make a little monster that you feed different color jellybeans that correspond with how you were feeling that day. This allows you to track how you've been feeling over a period of time and be mindful about why you've been feeling the way you are. </p>
+        <div class="row text-center">
+            <div class="col text-center">
+                <img src="MunchiesSketches.jpg" alt="" style="width:100%;">
+            </div>
+        </div>
+        </br>
         
-        <h5> <b> Project Guidelines </b></h5>
-
-        <h5> <b> The Problem </b></h5>
+        <h4> <b> Research </b></h4>
+        <p> To further define the product, I did some research into similar products in the market, different models of emotion and which age range this product could be most beneficial too. </p>
         
+       
+        <a href="https://eclkc.ohs.acf.hhs.gov/mental-health/article/fostering-emotional-literacy-young-children-labeling-emotions" target="blank" style="color:#48A2B5;">Fostering Emotional Literacy in Young Children: Labeling Emotions, U.S. Department of Health & Human Services</a>
+        <ul>
+            <li> <b>Emotional literacy</b> is the ability to identify, understand, and respond to emotions in oneself and others in a healthy manner.</li>
+            <li>Children who have a <b>strong foundation in emotional literacy</b> tolerate frustration better, get into fewer fights, and engage in less self-destructive behavior than those who don't. They are also healthier, less lonely, less impulsive, more focused, and they have greater academic achievement.</li>
+            <li>The development of a feeling word vocabulary is <b>critical in a child’s emotional development </b> because it makes it possible for children to better understand their emotional experiences and discuss and reflect with others about their personal experience of the world.</li>
+            <li> <b>Three variables in child’s ability to label emotions:</b> (1) the child’s temperament and developmental status, (2) parental socialization and environmental support, and (3) the teacher and child care providers’ emphasis on emotional literacy.</li>
+            <li> <b>Strategies to foster emotional literacy in children:</b> express your own feelings, label children's feelings, and play games, sing songs, and read stories with new feeling words.</li>
+        </ul>
 
-        <h3 class="text-center"> <b> Research </b> </h3>
+        <a href="https://doi-org.proxy.lib.umich.edu/10.1017/S095457940300018X" target="blank" style="color:#48A2B5;">First Grade Emotion Knowledge as a Predictor of Fifth Grade Self-Reported Internalizing Behaviors in Children from Economically Disadvantaged Families, Cambridge University Press</a>
+        <ul >
+            <li>Most children begin to <b>attach labels to emotions when they reach 18 months of age </b> and they become able to discuss feeling states they have experienced by 28 months. Emotion situation knowledge, the ability to infer the emotions of others through situational cues, develops slightly later.</li>
+            <li> The ability to label emotions on the basis of expressive and situational cues is <b>closely linked to verbal ability</b></li>
+            <li> <b>Differential emotions theory</b> states that each  emotion has a unique adaptive function that stems from its motivational quality. Emotions can be activated by cognitions, physiological states, or other emotions. As children develop, emotions and cognitions self-organize into patterns that can be activated by environmental events</li>
+            <li>Throughout the course of early and middle childhood, when connections between emotions and cognitions are developing, <b>repeated misperception of emotion cues may lead to inefficient or maladaptive emotion patterns</b> and emotion–cognition structures that activate negative emotions such as shame, guilt, sadness, and fear at inappropriate times</li>
+        </ul>
+        
+        <a href="https://www-jstor-org.proxy.lib.umich.edu/stable/1126703" style="color:#48A2B5;">A Circumplex Model for Social Behavior in Children, Child Development</a>
+        <ul>
+            <li>This model shows how different emotions are related. The x-axis of the model is the continuum between pleasant and unpleasant emotions. The y-axis is the continuum between high or low arousal or activation and deactivation.</li>
+        </ul>
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-4">
+                <img src="CircumplexModel.png" alt="" style="width:100%;">
+            </div>
+        </div>
+        </br>
+        <a href="https://www.digitaltrends.com/cool-tech/how-tamagotchi-shaped-tech/" target="blank" style="color:#48A2B5;">The Tamagotchi Effect: How digital pets shaped the tech habits of a generation, Digital Trends</a>
+        <ul>
+            <li>A Tamagotchi was, by design, supposed to simulate aliveness — complete with the messy realities that come with owning a pet</li>
+            <li> It was helpless without its users and, in return for our raising it, an emotional bond was formed.</li>
+        </ul>
+        </br>
+        
+        <h4> <b> Prototyping </b></h4>
+        <p>I haven't worked with clay in a very long time, so I did a lot of experimenting to make sure that the structure is strong and wouldn't crack when the clay is being baked. At the same time I was trying to make sure that the monster looked cute and had a big enough and easily accessible mouth to make sure it's functionable. After a few iterations, I decided on creating a rounded body with the mouth wide open and a big tongue sticking out. The monster has curved eyes, small ears and arms and stubby feet to add to the approachability, cuteness and vulnerability. </p>
+        <div class="row text-center">
+            <div class="col text-center">
+                <img src="MunchiesPrototype.jpg" alt="" style="width:100%;">
+            </div>
+        </div>
+        </br>
 
-        <h3 class="text-center"> <b> Design </b> </h3>
-        <h5> <b> Sketches </b></h5>
+        <h4> <b> Branding </b></h4>
+         <div class="row d-flex justify-content-center">
+            <div class="col-11 text-center">
+                <img src="MunchiesVisualIdentity.png" alt="" style="width:100%;">
+            </div>
+        </div>
 
-        <h5> <b> Prototype </b></h5>
+        <h4> <b> Packaging </b></h4>
+         <p>I wanted the packaging to show off the monster so that kids could see and interact with it on the shelves of a store and to create a home for it.</p>
 
-        <h5> <b> Packaging </b></h5>
+        <h4> <b> Final Product </b></h4>
+
+
+
 
     </div>
 </div>
